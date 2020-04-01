1104885e15e2451e1859e0c178d5d7f2bcef76f1
pie-bro <55726145+pie-bro@users.noreply.github.com>
2020-04-01 16:56:31 +1300

    Update SUM and COUNT.sql

--- SUM and COUNT.sql
+++ SUM and COUNT.sql
@@ -11,32 +11,32 @@ FROM world
 --2.
 --List all the continents - just once each.
 
-select distinct continent
-from world
+SELECT DISTINCT continent
+FROM world
 
 --GDP of Africa
 --3.
 --Give the total GDP of Africa
 
-select sum(GDP)
-from world
-where continent = 'africa'
+SELECT SUM(GDP)
+FROM world
+WHERE continent = 'africa'
 
 --Count the big countries
 --4.
 --How many countries have an area of at least 1000000
 
-select count(name)
-from world
-where area >=1000000
+SELECT COUNT(name)
+FROM world
+WHERE area >=1000000
 
 --Baltic states population
 --5.
 --What is the total population of ('Estonia', 'Latvia', 'Lithuania')
 
-select sum(population)
-from world
-where name in ('Estonia', 'Latvia', 'Lithuania')
+SELECT SUM(population)
+FROM world
+WHERE name IN ('Estonia', 'Latvia', 'Lithuania')
 
 --Using GROUP BY and HAVING
 --You may want to look at these examples: Using GROUP BY and HAVING.
@@ -45,25 +45,25 @@ where name in ('Estonia', 'Latvia', 'Lithuania')
 --6.
 --For each continent show the continent and number of countries.
 
-select continent, count(name)
-from world
-group by continent
+SELECT continent, COUNT(name)
+FROM world
+GROUP BY continent
 
 --Counting big countries in each continent
 --7.
 --For each continent show the continent and number of countries with populations of at least 10 million.
 
-select continent, count(name)
-from world
-where population >=10000000
-group by continent
+SELECT continent, COUNT(name)
+FROM world
+WHERE population >=10000000
+GROUP BY continent
 
 
 --Counting big continents
 --8.
 --List the continents that have a total population of at least 100 million.
 
-select continent
-from world
-group by continent
-having sum(population) >100000000
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) >100000000
