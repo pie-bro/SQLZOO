d651f29974278a02a21177731abf70f4f688e732
pie-bro <55726145+pie-bro@users.noreply.github.com>
2020-04-01 17:00:27 +1300

    Update Self join.sql

--- Self join.sql
+++ Self join.sql
@@ -1,15 +1,15 @@
 --1.
 --How many stops are in the database.
 
-select count(id)
-from stops
+SELECT COUNT(id)
+FROM stops
 
 --2.
 --Find the id value for the stop 'Craiglockhart'
 
-select id
-from stops 
-where name = 'Craiglockhart'
+SELECT id
+FROM stops 
+WHERE name = 'Craiglockhart'
 
 
 --3.
@@ -28,7 +28,7 @@ AND company = 'LRT'
 SELECT company, num, COUNT(*)
 FROM route WHERE stop=149 OR stop=53
 GROUP BY company, num
-having count(*)=2
+HAVING COUNT(*)=2
 
 --5.
 --Execute the self join shown and observe that b.stop gives all the places you can get to from Craiglockhart, without changing routes. Change the query so that it shows the services from Craiglockhart to London Road.
@@ -37,7 +37,7 @@ SELECT a.company, a.num, a.stop, b.stop
 FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
 WHERE a.stop=53
-and b.stop = 149
+AND b.stop = 149
 
 --6.
 --The query shown is similar to the previous one, however by joining two copies of the stops table we can refer to stops by name rather than by number. 
@@ -49,48 +49,48 @@ FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
-where a.company = 'SMT' order by a.company desc
+WHERE a.company = 'SMT' ORDER BY a.company DESC
 
 --Using a self join
 --7.
 --Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
 
-select distinct a.company, a.num 
-from route a join route b on (a.company=b.company AND a.num=b.num)
-where a.stop = 115 and b.stop = 137
+SELECT DISTINCT a.company, a.num 
+FROM route a JOIN route b ON (a.company=b.company AND a.num=b.num)
+WHERE a.stop = 115 AND b.stop = 137
 
 --8.
 --Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
 
-select distinct a.company, a.num 
-from route a join route b on (a.company=b.company AND a.num=b.num)
-join stops stopa on (a.stop = stopa.id)
-join stops stopb on (b.stop = stopb.id)
-where stopa.name = 'Craiglockhart' and stopb.name = 'Tollcross'
+SELECT DISTINCT a.company, a.num 
+FROM route a JOIN route b ON (a.company=b.company AND a.num=b.num)
+JOIN stops stopa ON (a.stop = stopa.id)
+JOIN stops stopb ON (b.stop = stopb.id)
+WHERE stopa.name = 'Craiglockhart' AND stopb.name = 'Tollcross'
 
 --9.
 --Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, 
 offered by the LRT company. Include the company and bus no. of the relevant services.
 
-select distinct stopb.name, a.company,a.num
-from route a join route b on (a.company=b.company AND a.num=b.num)
-join stops stopa on (a.stop = stopa.id)
-join stops stopb on (b.stop = stopb.id)
-where stopa.name = 'Craiglockhart'
+SELECT DISTINCT stopb.name, a.company,a.num
+FROM route a JOIN route b ON (a.company=b.company AND a.num=b.num)
+JOIN stops stopa ON (a.stop = stopa.id)
+JOIN stops stopb ON (b.stop = stopb.id)
+WHERE stopa.name = 'Craiglockhart'
 
 --10.
 --Find the routes involving two buses that can go from Craiglockhart to Lochend.
 --Show the bus no. and company for the first bus, the name of the stop for the transfer,
 --and the bus no. and company for the second bus.
 
-select distinct a.num, a.company,stopb.name,c.num,c.company
-from route a 
-join route b on (a.company=b.company AND a.num=b.num)
-join (route c join route d on (c.company=d.company AND c.num=d.num))
-join stops stopa on (a.stop = stopa.id)
-join stops stopb on (b.stop = stopb.id)
-join stops stopc on (c.stop = stopc.id)
-join stops stopd on (d.stop = stopd.id)
-where stopa.name = 'Craiglockhart'
-and stopd.name = 'Lochend'
-and stopb.name = stopc.name
+SELECT DISTINCT a.num, a.company,stopb.name,c.num,c.company
+FROM route a 
+JOIN route b ON (a.company=b.company AND a.num=b.num)
+JOIN (route c JOIN route d ON (c.company=d.company AND c.num=d.num))
+JOIN stops stopa ON (a.stop = stopa.id)
+JOIN stops stopb ON (b.stop = stopb.id)
+JOIN stops stopc ON (c.stop = stopc.id)
+JOIN stops stopd ON (d.stop = stopd.id)
+WHERE stopa.name = 'Craiglockhart'
+AND stopd.name = 'Lochend'
+AND stopb.name = stopc.name
