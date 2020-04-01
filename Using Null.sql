6e6b484c796826481a5fca6da825534a364fb1c0
pie-bro <55726145+pie-bro@users.noreply.github.com>
2020-04-01 17:11:17 +1300

    Update Using Null.sql

--- Using Null.sql
+++ Using Null.sql
@@ -24,7 +24,7 @@ SELECT teacher.name, dept.name
 --Use a different JOIN so that all teachers are listed.
 
 SELECT teacher.name, dept.name
- FROM teacher left JOIN dept
+ FROM teacher LEFT JOIN dept
            ON (teacher.dept=dept.id)
 
 
@@ -32,7 +32,7 @@ SELECT teacher.name, dept.name
 --Use a different JOIN so that all departments are listed.
 
 SELECT teacher.name, dept.name
- FROM teacher right JOIN dept
+ FROM teacher RIGHT JOIN dept
            ON (teacher.dept=dept.id)
 
 
@@ -42,47 +42,47 @@ SELECT teacher.name, dept.name
 --Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. 
 Show teacher name and mobile number or '07986 444 2266'
 
-select t.name, coalesce(t.mobile, '07986 444 2266')
-from teacher t left join dept d on dept = d.id
+SELECT t.name, coalesce(t.mobile, '07986 444 2266')
+FROM teacher t left JOIN dept d ON dept = d.id
 
 --6.
 --Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. 
 Use the string 'None' where there is no department.
 
-select t.name, coalesce(d.name, 'None')
-from teacher t left join dept d on dept = d.id
+SELECT t.name, coalesce(d.name, 'None')
+FROM teacher t left JOIN dept d ON dept = d.id
 
 
 --7.
 --Use COUNT to show the number of teachers and the number of mobile phones.
 
-select count(name), count(mobile)
-from teacher
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher
 
 --8.
 --Use COUNT and GROUP BY dept.name to show each department and the number of staff. 
 Use a RIGHT JOIN to ensure that the Engineering department is listed.
 
-select d.name, count(t.name)
-from teacher t right join dept d on t.dept = d.id 
-group by d.name
+SELECT d.name, COUNT(t.name)
+FROM teacher t right JOIN dept d ON t.dept = d.id 
+GROUP BY d.name
 
 --Using CASE
 
 --9.
 --Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
 
-select teacher.name, case when dept.id =1 then 'Sci'
-when dept.id =2 then 'Sci'
-else 'Art' end
-from teacher left join dept on teacher.dept = dept.id
+SELECT teacher.name, CASE WHEN dept.id =1 THEN 'Sci'
+WHEN dept.id =2 THEN 'Sci'
+ELSE 'Art' END
+FROM teacher LEFT JOIN dept ON teacher.dept = dept.id
 
 
 --10.
 --Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
 
-select teacher.name, case when dept.id =1 then 'Sci'
-when dept.id =2 then 'Sci'
-when dept.id =3 then 'Art'
-else 'None' end
-from teacher left join dept on teacher.dept = dept.id
+SELECT teacher.name, CASE WHEN dept.id =1 THEN 'Sci'
+WHEN dept.id =2 THEN 'Sci'
+WHEN dept.id =3 THEN 'Art'
+ELSE 'None' END
+FROM teacher LEFT JOIN dept ON teacher.dept = dept.id
