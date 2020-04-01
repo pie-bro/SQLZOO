887e37c21f34e3cc936e58b5bac9393bbf9da960
pie-bro <55726145+pie-bro@users.noreply.github.com>
2020-04-01 17:04:59 +1300

    Update The JOIN operation.sql

--- The JOIN operation.sql
+++ The JOIN operation.sql
@@ -150,7 +150,7 @@ team1,
 SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1, 
 team2, 
 SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END ) score2
-FROM game left JOIN goal ON matchid = id
+FROM game LEFT JOIN goal ON matchid = id
 GROUP BY mdate, team1,team2
 ORDER BY  mdate, matchid, team1,team2
   
