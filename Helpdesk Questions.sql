48e63fd87637e541724062e9a19056136079cdcc
pie-bro <55726145+pie-bro@users.noreply.github.com>
2020-07-28 15:03:52 +1200

    Create Helpdesk Questions.sql

---
+++ Helpdesk Questions.sql
@@ -0,0 +1,80 @@
+--Helpdesk Easy Questions
+
+--1.There are three issues that include the words "index" and "Oracle". Find the call_date for each of them
+
+SELECT call_date, call_ref FROM Issue
+WHERE Detail LIKE '%index%'
+AND Detail LIKE '%Oracle%'
+
+
+--2.Samantha Hall made three calls on 2017-08-14. Show the date and time for each
+
+SELECT call_date, first_name, last_name
+FROM Caller c JOIN Issue i ON c.Caller_id = i.Caller_id
+WHERE First_name = 'Samantha' AND last_name = 'Hall'
+AND Call_date LIKE '%2017-08-14%'
+
+
+--3.There are 500 calls in the system (roughly). Write a query that shows the number that have each status.
+
+SELECT status, count(*) Volume
+FROM Issue
+GROUP BY status
+
+
+--4.Calls are not normally assigned to a manager but it does happen. How many calls have been assigned to staff who are at Manager Level?
+
+SELECT count(*)
+FROM Issue 
+JOIN Staff s ON Assigned_to = Staff_code
+JOIN Level l ON s.Level_code = l.Level_code
+WHERE l.Manager IS NOT NULL
+
+
+--5.Show the manager for each shift. Your output should include the shift date and type; also the first and last name of the manager.
+
+SELECT Shift_date, Shift_type, First_name, Last_name
+FROM Shift sh
+JOIN Staff s ON sh.Manager = s.Staff_code
+ORDER BY 1
+
+
+--Helpdesk Medium Questions
+
+--6.List the Company name and the number of calls for those companies with more than 18 calls.
+
+SELECT Company_name, COUNT(*)
+FROM Customer cu
+JOIN Caller c ON cu.Company_ref = c.Company_ref
+JOIN Issue i ON c.Caller_id = i.Caller_id
+GROUP BY 1
+HAVING COUNT(*) > 18
+
+--7.Find the callers who have never made a call. Show first name and last name
+
+SELECT First_name, Last_name
+FROM Caller
+WHERE Caller_id NOT IN
+(SELECT Caller_id
+FROM  Issue 
+GROUP BY 1)
+
+--**8.For each customer show: Company name, contact name, number of calls where the number of calls is fewer than 5
+SELECT Company_name, First_name, Last_name, nc
+FROM
+(SELECT Company_name, Contact_id, Count(*) nc
+FROM Issue i
+JOIN Caller c ON c.Caller_id = i.Caller_id
+JOIN Customer cu ON cu.Company_ref = c.Company_ref
+GROUP BY 1,2
+HAVING nc < 5 ) e
+JOIN
+(SELECT Caller_id, First_name, Last_name
+FROM Caller
+) f
+ON 
+e.Contact_id = f.Caller_id
+
+
+
+
